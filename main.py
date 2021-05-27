a7c6f7a16faa852608814d4d60eadc52bdc7dcb7
DTNomad <84250615+DTNomad@users.noreply.github.com>
2021-05-27 07:18:33 -0700

    Feat. Play Invite
    Add feature to collect reactions from users to notify who wants to play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import asyncio
 
 client = discord.Client()
 
@@ -9,11 +10,20 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-  
-  if message.content.startswith('!d a'):
-    text = '<@' + str(message.author.id) + '>'
-    await message.channel.send(text)
+  if message.content.startswith('!dinv '):
+    channel = message.channel
+    user_text = message.author.name
+    game_text = message.content[6:]
+    await channel.send(user_text + ' wants to play **' + game_text + '**. 👍 to join!')
+
+    def check(reaction, user):
+      return user == message.author and str(reaction.emoji) == '👍'
+
+    try:
+      reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+    except asyncio.TimeoutError:
+      await channel.send('👎')
+    else:
+      await channel.send(user.name + ' wants to join')
 
 client.run(os.environ['TOKEN'])
